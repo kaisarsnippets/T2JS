c7610c272ce946b61f199a81ef7b85c9e9e330d7
KaisarCode <kaisar@kaisarcode.com>
2020-10-17 12:56:45 -0300

    Revisit code

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/**
- * T2JS - [NodeJS] String templates to JS
- * 
- * @param (string) - str: Template string
- * @param (object) - cfg: Configuration object
- * */
+// T2JS - [NodeJS] String templates to JS
 var tagfun = require('kc-tagfun');
 var regesc = require('kc-regesc');
 var rmcomm = require('kc-rmcomm');
 var strmin = require('kc-strmin');
 module.exports = function(str, cfg) {
+    var rx;
     
     // IO String
     str = str || '';
@@ -40,39 +36,39 @@ module.exports = function(str, cfg) {
     // Avoid consecutive js blocks
     var j1e = regesc(j1);
     var j2e = regesc(j2);
-    var rx = j2e+'[\\s]*?'+j1e;
+    rx = j2e+'[\\s]*?'+j1e;
     rx = new RegExp(rx, 'gm');
     str = str.replace(rx, '');
     
     // Avoid consecutive literals
     var s1e = regesc(s1);
     var s2e = regesc(s2);
-    var rx = s2e+'[\\s]*?'+s1e;
+    rx = s2e+'[\\s]*?'+s1e;
     rx = new RegExp(rx, 'gm');
     str = str.replace(rx, '+""+');
     
     // Parse JS blocks
     str = tagfun(str, j2, j1, function(c){
-    cfg.mini?
-    c = c.replace(/\n/gm, ''):
-    c = c.replace(/\n/gm, '\\n');
-    return "'"+strmin(c)+"'"; });
-    str = tagfun(str, j1, j2, function(c){
-    return c; });
+        cfg.mini?
+        c = c.replace(/\n/gm, ''):
+        c = c.replace(/\n/gm, '\\n');
+        
+        // Parse string literals
+        c = tagfun(c, s1, s2, function(c){
+            return "'+("+c+")+'"
+        }); return "'"+strmin(c)+"'";
+    });
     
-    // String literals
-    str = tagfun(str, s1, s2, function(c){
-    cfg.mini?
-    c = c.replace(/\n/gm, ''):
-    c = c.replace(/\n/gm, '\\n');
-    return "'+("+strmin(c)+")+'"; });
-    str = tagfun(str, "''+", "+''", function(c){
-    return "\\''+"+c+"+'\\'"; });
+    // Remove first and last tags
+    str = tagfun(str, j1, j2, function(c){
+        return c;
+    });
     
     // Cleanup
     if (cfg.mini) str = rmcomm(str);
     if (cfg.mini) str = strmin(str);
     str = str.trim();
+    str = str.replace(/\+''/g,'');
     
     // OUT
     return str;
